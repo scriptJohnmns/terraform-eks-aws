312eaba400e8d8c09a4a6a5a31afa2d98e959dfc
scriptJohnmns <johnmnavess@gmail.com>
2025-10-06 11:49:35 -0300

    feat: add remote backend s3 bucket

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.15.0"
     }
   }
 }
-
+terraform {
+  backend "s3" {
+    bucket = "joaodevops-bucket01"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
